55266e4627917af3d7c689bf0083a3c81d0f3dc3
serraogt <85106072+serraogt@users.noreply.github.com>
2023-08-01 09:32:50 +0300

    logo not shown

--- src/App.js
+++ src/App.js
@@ -6,14 +6,27 @@ import productData from './data/productData';
 import Searchbar from './components/Searchbar';
 
 
+
 function App() {
  
   return (
     <div className="App">
     
       <img src={logo} className="App-logo" alt="logo" width={500} />
-      <Searchbar/>
-      
+      <Searchbar className="sb"/>
+      <div className='container'>
+        {productData.map((product,id) => (  
+          <div>
+          <Product 
+            key={id}
+            productPic={product.productPic}
+            productName={product.productName}
+            price={product.price}
+            additional={product.additional}/> 
+          <br/>
+          </div>
+            )) }
+         </div> 
     </div> 
       );
 }
